92e730d108e04f22254438f20322b53addfd72f2
Sapirrior <snappyyxd@gmail.com>
2024-07-20 11:16:38 +0530

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,7 +1,7 @@
 // Copyright © 2024 Sapirrior
 const { ShardingManager } = require('discord.js');
 
-const manager = new ShardingManager('./aura.js', { token: 'YOUR_BOT_TOKEN_HERE' });
+const manager = new ShardingManager('./scuttler.js', { token: 'YOUR_BOT_TOKEN_HERE' });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
